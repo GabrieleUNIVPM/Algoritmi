a8d0fb3fe072fcd319c09c73d318ed2ea72aa5bc
Gabriele <g.debartolomeo09@gmail.com>
2018-08-13 12:15:00 +0200

    Function.h function redefinition fixed, Trapezi.h filled

--- Trapezi.h
+++ Trapezi.h
@@ -7,9 +7,54 @@
 
 using namespace std;
 
-void Trapezi(/*funzione da integrare*/){
+void Trapezi()
+
+{
 	
 	cout<<"\n\nIn esecuzione integrazione con IL METODO DEI TRAPEZI\n "<<endl;
 	
-	
+ double h , a , b ,somma;
+ int n ;
+
+
+ 
+ 
+ do {
+ cout<<"\n Limite inferiore d'integrazione : ";
+ cin>>a;
+ cout<<"\n Limite superiore d'integrazione : ";
+ cin>>b ;
+ 
+ if (a>=b)
+ cout<<"\n Errore: a deve essere minore di b, RIPROVARE: ";
+ 
+ } while ( a >= b) ;
+ 
+ 
+ 
+ cout<< "Numero di intervalli\t ";
+ cin>>n ;
+ 
+ h=(b-a)/n ;		//ampiezza degli intervalli
+ 
+ 
+ somma=( f ( a )+f (b) )*0.5 ; /*Valore approssimato dell'integrale*/
+ 
+ 
+ for (int i=1; i<n; i++)
+ 
+ somma += f (a+ i*h) ; /* Calcolo di f ( x ) e somma all'area */
+ somma *= h;
+ 
+ 
+ cout<<"L' integrale vale \t"<<somma;
+
+
+
 }
+
+
+
+
+ 
+
